29a60f66e6c6f4c25761261941ff03fe71fb6167
agus <agusalta002@gmail.com>
2024-06-06 22:28:51 -0300

    add 3 busquedas a la api

--- services/api.js
+++ services/api.js
@@ -1,2 +1,38 @@
 // Buscar bebida por nombre
-// URL = 'www.thecocktaildb.com/api/json/v1/1/search.php?s='
+// URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+export const searchCocktailByName = async (name) => {
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        const cocktail = await response.json();
+        console.log(cocktail);
+        return cocktail;
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+// Buscar bebida por la primera letra
+// URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
+export const searchCocktailByFirstLetter = async (letter) => {
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
+        const cocktail = await response.json();
+        console.log(cocktail);
+        return cocktail;
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+// Buscar ingrediente por el nombre
+// URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i='
+export const searchIngredientByName = async (name) => {
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`);
+        const ingredient = await response.json();
+        console.log(ingredient);
+        return ingredient;
+    } catch (err) {
+        console.log(err.message);
+    }
+}
